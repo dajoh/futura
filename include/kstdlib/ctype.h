5507b5218a7b4767adaceb844bf0c90796d5a638
Daniel Lindberg <dajoh10@gmail.com>
2020-12-22 19:49:27 +0100

    kstdlib: implemented tolower and toupper

--- include/kstdlib/ctype.h
+++ include/kstdlib/ctype.h
@@ -1,11 +1,6 @@
 #ifndef __CTYPE_H__
 #define __CTYPE_H__
 
-/* TODO: implement
-static inline int tolower(int c);
-static inline int toupper(int c);
-*/
-
 static inline int isdigit(int c)  { return (c >= '0' && c <= '9'); }
 static inline int islower(int c)  { return (c >= 'a' && c <= 'z'); }
 static inline int isupper(int c)  { return (c >= 'A' && c <= 'Z'); }
@@ -18,5 +13,7 @@ static inline int iscntrl(int c)  { return (c >= 0 && c <= 31) || c == 127; }
 static inline int ispunct(int c)  { return (c >= 33 && c <= 47) || (c >= 58 && c <= 64) || (c >= 91 && c <= 96) || (c >= 123 && c <= 126); }
 static inline int isgraph(int c)  { return isalnum(c) || ispunct(c); }
 static inline int isprint(int c)  { return c == ' ' || isalnum(c) || ispunct(c); }
+static inline int tolower(int c)  { return isupper(c) ? c + 32 : c; }
+static inline int toupper(int c)  { return islower(c) ? c - 32 : c; }
 
 #endif
